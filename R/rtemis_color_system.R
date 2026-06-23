7ad3b86bbb7d3f0cf0decae4a0886af53dc0ea25
Stathis Gennatas <gennatas@gmail.com>
2026-06-23 07:01:57 -0700

    up colors

--- R/rtemis_color_system.R
+++ R/rtemis_color_system.R
@@ -16,25 +16,27 @@ rtemis_colors <- c(
   light_orange = "#FDB808",
   orange = "#F08904",
   magenta = "#BE2E5F",
-  green = "#526551",
+  green = "#0F6A66",
+  juniper = "#526551",
   light_blue = "#B3CFE8",
   blue = "#466D96",
   dark_blue = "#375D86",
   red = "#EA384A",
-  light_mauve = "#ECBDC3",
+  light_mauve = "#B1A7B3",
   pink = "#F384FF",
   purple = "#7364F2",
-  dark_magenta = "#7D0830"
+  dark_magenta = "#7D0830",
+  terracotta = "#895140"
 )
 
 # Internal colors
-col_highlight <- rtemis_colors[["orange"]]
+col_highlight <- rtemis_colors[["teal"]]
 col_suggest <- rtemis_colors[["orange"]]
-col_object <- rtemis_colors[["green"]]
+col_object <- rtemis_colors[["teal"]]
 
 # Log-level colors — used by info() / warn() / success() / debug() / abort()
 col_info <- rtemis_colors[["blue"]]
 col_warn <- rtemis_colors[["light_orange"]]
 col_error <- rtemis_colors[["red"]]
-col_success <- rtemis_colors[["green"]]
+col_success <- "#00cc8f"
 col_debug <- "#808080"
